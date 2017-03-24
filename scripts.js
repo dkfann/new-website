9172350e48e01a98833e971867f04b4e62e50dad
dkfann <danny.k.fann@gmail.com>
2017-03-24 09:33:45 -0700

    Added showcase detail with some animations

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 $(() => {
+    let activeDetailPanel = '';
     // CSS Only Approach - Revisit Later
     // $('.c-panel__photo').on('mouseenter', event => {
     //     $(event.currentTarget).addClass('is-hovered');
@@ -38,11 +39,54 @@ $(() => {
         });
     });
 
+    // $('.c-panel__photo').on('click', event => {
+    //     $(event.currentTarget).toggleClass('is-selected');
+    //     $('.c-panel__photo')
+    //         .not($(event.currentTarget))
+    //         .parents('.c-panel')
+    //         .addClass('is-hidden');
+    // });
+
     $('.c-panel__photo').on('click', event => {
-        $(event.currentTarget).toggleClass('is-selected');
-        $('.c-panel__photo')
-            .not($(event.currentTarget))
-            .parents('.c-panel')
-            .addClass('is-hidden');
+        // $('.c-showcase-detail--left').addClass('is-active');
+        const elem = $('.c-showcase-detail--left');
+        const panelBgColor = $(event.currentTarget).css('background-color');
+        elem.css({
+            backgroundColor: panelBgColor,
+        });
+        TweenLite.to(elem.get(0), 0.5, {
+            left: '0',
+            ease: Cubic.easeInOut,
+        });
+
+        activeDetailPanel = 'left';
+        // const target = $(event.currentTarget);
+        // var offset_t = $(event.currentTarget).offset().top - $(window).scrollTop();
+        // var offset_l = $(event.currentTarget).offset().left - $(window).scrollLeft();
+
+        // var left = Math.round( (event.clientX - offset_l) );
+        // var top = Math.round( (event.clientY - offset_t) );
+
+        // console.log("Left: " + left + " Top: " + top);
+
+        // const xform = anime({
+        //     targets: target.get(0),
+        //     duration: 1000,]
+        //     scale: 2,
+        // });
+    });
+
+    $('.c-close').on('click', event => {
+        if (activeDetailPanel === 'left') {
+            const elem = $('.c-showcase-detail--left');
+            TweenLite.to(elem.get(0), 0.5, {
+                left: '-100vw',
+                ease: Cubic.easeInOut,
+            });
+
+            activeDetailPanel = '';
+        }
+        // $('.c-showcase-detail--left').removeClass('is-active');
+        // $('.c-showcase-detail--right').removeClass('is-active');
     });
 })
