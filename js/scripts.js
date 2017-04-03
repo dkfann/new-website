a8858b125b3fab4965e6599ee7d0577f79016044
dkfann <danny.k.fann@gmail.com>
2017-04-03 01:08:10 -0700

    Refactored js, compiled less, fixed some issues with screen sizes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,253 @@
 $(() => {
-    let activeDetailPanel = '';
-    let pswpGallery = null;
+    function pageScripts() {
+        let activeDetailPanel = '';
+        let pswpGallery = null;
+        const panelData = getPanelData();
 
-    handleSelfInfoPosition();
+        // Initialize the selected panel data as null to be populated later
+        let panelName = '';
+        let selectedPanelData = null;
+        let showcaseDetailElem = $('.c-showcase-detail--left');
 
-    $(window).on('resize', () => {
-        handleSelfInfoPosition();
-    })
+        function handleSelfInfoPosition() {
+            const pos = $('#recon-panel').position();
+            $('.c-self-info').css({
+                marginLeft: `${pos.left}px`,
+            });
 
-    function handleSelfInfoPosition() {
-        const pos = $('#recon-panel').position();
-        $('.c-self-info').css({
-            marginLeft: `${pos.left}px`,
-        });
+            $(window).on('resize', () => {
+                handleSelfInfoPosition();
+            });
+        }
+
+        function initGallery({ index }) {
+            const gallery = [
+                {
+                    src: '../resources/images/kb.jpg',
+                    w: 1000,
+                    h: 667,
+                },
+                {
+                    src: '../resources/images/peppermint.jpg',
+                    w: 1000,
+                    h: 563,
+                },
+                {
+                    src: '../resources/images/beach.jpg',
+                    w: 1000,
+                    h: 663,
+                },
+                {
+                    src: '../resources/images/car.jpg',
+                    w: 1000,
+                    h: 663,
+                },
+                {
+                    src: '../resources/images/dance.jpg',
+                    w: 1000,
+                    h: 663,
+                },
+            ];
+
+            const pswpElem = $('.pswp').get(0);
+            const pswpOpts = {
+                closeOnScroll: false,
+                index,
+            };
+
+            pswpGallery = new PhotoSwipe(pswpElem, PhotoSwipeUI_Default, gallery, pswpOpts);
+        }
+
+        function handlePanelClick() {
+            $('.c-panel__photo').on('click', event => {
+
+                if (showcaseDetailElem.find('.c-showcase-detail__info').length) return;
+
+                positionShowcaseElemOnScreen();
+                updatePanelBgColor();
+                getPanelName({ event });
+                getPanelInfoFromName();
+                addShowcaseDetailInfo();
+
+                if (selectedPanelData.panelGithub) {
+                    handlePanelsWithGithub();
+                }
+                if (selectedPanelData.panelImg) {
+                    handlePanelsWithImages();
+                }
+                if (panelName === 'photo') {
+                    handlePhotoPanelCase();
+                }
+                if (panelName === 'about') {
+                    handleAboutPanelColors();
+                }
+                else {
+                    handleDefaultPanelColors();
+                }
+
+                moveShowcaseElemIn();
+
+                $('body').toggleClass('no-scroll');
+            });
+        }
+
+        function handlePanelEnter() {
+            $('.c-panel__photo').on('mouseenter', event => {
+                const title = $(event.currentTarget).find('.c-panel__desc');
+                const image = $(event.currentTarget);
+                image.addClass('is-hovered');
+                TweenLite.to(title.get(0), 0.5, {
+                    top: '50%',
+                    opacity: '1',
+                    ease: Cubic.easeInOut,
+                });
+            });
+        }
+
+        function handlePanelLeave() {
+            $('.c-panel__photo').on('mouseleave', event => {
+                const title = $(event.currentTarget).find('.c-panel__desc');
+                $(event.currentTarget).removeClass('is-hovered');
+                TweenLite.to(title.get(0), 0.5, {
+                    top: '70%',
+                    opacity: '0',
+                    ease: Cubic.easeInOut,
+                });
+            });
+        }
+
+        function updatePanelBgColor() {
+            const panelBgColor = $(event.currentTarget).css('background-color');
+            showcaseDetailElem.css({
+                backgroundColor: panelBgColor,
+            });
+        }
+
+        function getPanelName({ event }) {
+            const id = $(event.currentTarget).get(0).id;
+            panelName = id.split('-')[0];
+        }
+
+        function getPanelInfoFromName() {
+            selectedPanelData = {
+                panelTitle: panelData[panelName].title,
+                panelInfo: panelData[panelName].text,
+                panelGithub: panelData[panelName].github,
+                panelImg: panelData[panelName].img,
+                panelGallery: panelData[panelName].gallery,
+            };
+        }
+
+        function addShowcaseDetailInfo() {
+            showcaseDetailElem.append(`
+                <div class="c-showcase-detail__info">
+                    <div class="c-showcase-detail__info-title">
+                        ${selectedPanelData.panelTitle}
+                    </div>
+                    <div class="c-showcase-detail__info-text">
+                        ${selectedPanelData.panelInfo}
+                    </div>
+                </div>
+            `);
+        }
+
+        function positionShowcaseElemOnScreen() {
+            // Position the element to cover your screen if you had to scroll
+            const scrollOffset = $(window).scrollTop();
+
+            showcaseDetailElem.css({
+                top: scrollOffset,
+            });
+        }
+
+        function handlePanelsWithGithub() {
+            $(`
+                <div class="c-showcase-detail__github">
+                    <img src="../resources/icons/github.png" alt="" data-url="${selectedPanelData.panelGithub}"/>
+                </div>
+            `).insertAfter('.c-showcase-detail__info-text');
+
+            $('.c-showcase-detail__github').on('click', function() {
+                window.location.href = $(this).data('url');
+            });
+        }
+
+        function handlePanelsWithImages() {
+            $(`
+                <div class="c-showcase-detail__img">
+                    <img src="${selectedPanelData.panelImg}"/>
+                <div>
+            `).insertBefore('.c-showcase-detail__info-text');
+        }
+
+        function handlePhotoPanelCase() {
+            const galleryMarkup = selectedPanelData.panelGallery;
+
+            $(galleryMarkup).insertAfter('.c-showcase-detail__info-title');
+
+            $('.c-showcase-detail__info').addClass('is-photo-panel');
+
+            $('.c-showcase-detail__gallery-photo').on('click', event => {
+                const index = $(event.currentTarget).data('id');
+                initGallery({ index });
+                pswpGallery.init();
+            });
+        }
+
+        function handleAboutPanelColors() {
+            $('.c-showcase-detail--left').css({
+                color: '#FFF',
+            });
+        }
+
+        function handleDefaultPanelColors() {
+            $('.c-showcase-detail--left').css({
+                color: '#000',
+            });
+        }
+
+        function moveShowcaseElemIn() {
+            TweenLite.to(showcaseDetailElem.get(0), 0.5, {
+                left: '0',
+                ease: Cubic.easeInOut,
+            });
+        }
+
+        function moveShowcaseElemOut() {
+            TweenLite.to(showcaseDetailElem.get(0), 0.5, {
+                left: '-100vw',
+                ease: Cubic.easeInOut,
+                onComplete: _removeDetails.bind(this, showcaseDetailElem),
+            });
+        }
+
+        function handlePanelClose() {
+            $('.c-close').on('click', event => {
+                moveShowcaseElemOut();
+                $('body').toggleClass('no-scroll');
+            });
+        }
+
+        function _removeDetails(elem) {
+            showcaseDetailElem.find('.c-showcase-detail__info').remove();
+        }
+
+        function init() {
+            handleSelfInfoPosition();
+            handlePanelEnter();
+            handlePanelLeave();
+            handlePanelClick();
+            handlePanelClose();
+        }
+
+        init();
     }
 
-    function initGallery({ index }) {
-        const gallery = [
-            {
-                src: '../resources/images/kb.jpg',
-                w: 1000,
-                h: 667,
-            },
-            {
-                src: '../resources/images/peppermint.jpg',
-                w: 1000,
-                h: 563,
-            },
-            {
-                src: '../resources/images/beach.jpg',
-                w: 1000,
-                h: 663,
-            },
-            {
-                src: '../resources/images/car.jpg',
-                w: 1000,
-                h: 663,
-            },
-            {
-                src: '../resources/images/dance.jpg',
-                w: 1000,
-                h: 663,
-            },
-        ];
+    pageScripts();
+});
 
-        const pswpElem = $('.pswp').get(0);
-        const pswpOpts = {
-            closeOnScroll: false,
-            index,
-        };
-
-        pswpGallery = new PhotoSwipe(pswpElem, PhotoSwipeUI_Default, gallery, pswpOpts);
-    }
-
-    const panelData = {
+function getPanelData() {
+    return {
         rfid: {
             title: 'RFID Electric Strike Lock',
             text: 'An RFID lock system created by connecting an ATmega32 microprocessor with an RFID chip. The RFID chip reads in a keycard on UART, sends the unique serial identification code to the microprocessor, then opens the lock if the keycard\'s ID is accepted.',
@@ -112,133 +306,5 @@ $(() => {
             text: 'A restaurant sharing app that helps friends share and discover a curated assortment of places to eat and hang out at. Created with React, React Router, Webpack, and Babel.',
         },
     }
-    // CSS Only Approach - Revisit Later
-    // $('.c-panel__photo').on('mouseenter', event => {
-    //     $(event.currentTarget).addClass('is-hovered');
-    // });
+}
 
-    // $('.c-panel__photo').on('mouseleave', event => {
-    //     $(event.currentTarget).removeClass('is-hovered');
-    // });
-
-    $('.c-panel__photo').on('mouseenter', event => {
-        const title = $(event.currentTarget).find('.c-panel__desc');
-        const image = $(event.currentTarget);
-        image.addClass('is-hovered');
-        TweenLite.to(title.get(0), 0.5, {
-            top: '50%',
-            opacity: '1',
-            ease: Cubic.easeInOut,
-        });
-    });
-
-    $('.c-panel__photo').on('mouseleave', event => {
-        const title = $(event.currentTarget).find('.c-panel__desc');
-        $(event.currentTarget).removeClass('is-hovered');
-        TweenLite.to(title.get(0), 0.5, {
-            top: '70%',
-            opacity: '0',
-            ease: Cubic.easeInOut,
-        });
-    });
-
-    $('.c-panel__photo').on('click', event => {
-        const elem = $('.c-showcase-detail--left');
-        if (elem.find('.c-showcase-detail__info').length) return;
-        const panelBgColor = $(event.currentTarget).css('background-color');
-        const id = $(event.currentTarget).get(0).id;
-        const panelName = id.split('-')[0];
-
-        const panelTitle = panelData[panelName].title;
-        const panelInfo = panelData[panelName].text;
-        const panelGithub = panelData[panelName].github;
-        const panelImg = panelData[panelName].img;
-
-        elem.append(`
-            <div class="c-showcase-detail__info">
-                <div class="c-showcase-detail__info-title">
-                    ${panelTitle}
-                </div>
-                <div class="c-showcase-detail__info-text">
-                    ${panelInfo}
-                </div>
-            </div>
-        `);
-    
-        if (panelGithub) {
-            $(`
-                <div class="c-showcase-detail__github">
-                    <img src="../resources/icons/github.png" alt="" data-url="${panelGithub}"/>
-                </div>
-            `).insertAfter('.c-showcase-detail__info-text');
-
-            $('.c-showcase-detail__github').on('click', function() {
-                window.location.href = $(this).data('url');
-            });
-        }
-
-        if (panelImg) {
-            $(`
-                <div class="c-showcase-detail__img">
-                    <img src="${panelImg}"/>
-                <div>
-            `).insertBefore('.c-showcase-detail__info-text');
-        }
-
-        elem.css({
-            backgroundColor: panelBgColor,
-        });
-
-        if (panelName === 'photo') {
-            const galleryMarkup = panelData[panelName].gallery;
-
-            $(galleryMarkup).insertAfter('.c-showcase-detail__info-title');
-
-            $('.c-showcase-detail__gallery-photo').on('click', event => {
-                const index = $(event.currentTarget).data('id');
-                initGallery({ index });
-                pswpGallery.init();
-            });
-        }
-
-        if (panelName === 'about') {
-            $('.c-showcase-detail--left').css({
-                color: '#FFF',
-            });
-        }
-        else {
-            $('.c-showcase-detail--left').css({
-                color: '#000',
-            });
-        }
-
-        TweenLite.to(elem.get(0), 0.5, {
-            left: '0',
-            ease: Cubic.easeInOut,
-        });
-
-        activeDetailPanel = 'left';
-    });
-
-    $('.c-close').on('click', event => {
-        if (activeDetailPanel === 'left') {
-            const elem = $('.c-showcase-detail--left');
-            TweenLite.to(elem.get(0), 0.5, {
-                left: '-100vw',
-                ease: Cubic.easeInOut,
-                onComplete: _removeDetails.bind(this, elem),
-            });
-
-            activeDetailPanel = '';
-        }
-    });
-
-    function _getInfoToShowFromPanelName({ info }) {
-
-    }
-
-    function _removeDetails(elem) {
-        console.log('Details have been removed');
-        elem.find('.c-showcase-detail__info').remove();
-    }
-})
